4fe1bc2ae27ea8ea844d05748056509ef595f217
tamomsi <122281591+tamomsi@users.noreply.github.com>
2024-05-11 17:25:53 +0200

    added a new method findByUserIdAndDate with the parameters userId and date

--- src/main/java/com/fantasy/tbs/repository/TimeBookingRepository.java
+++ src/main/java/com/fantasy/tbs/repository/TimeBookingRepository.java
@@ -3,10 +3,15 @@ package com.fantasy.tbs.repository;
 import com.fantasy.tbs.domain.TimeBooking;
 import org.springframework.data.jpa.repository.*;
 import org.springframework.stereotype.Repository;
+import java.time.LocalDate;
+import java.util.List;
 
 /**
  * Spring Data SQL repository for the TimeBooking entity.
  */
 @SuppressWarnings("unused")
 @Repository
-public interface TimeBookingRepository extends JpaRepository<TimeBooking, Long> {}
+public interface TimeBookingRepository extends JpaRepository<TimeBooking, Long> {
+    // TODO: Adjust the method to properly query the database for time bookings based on the user ID and date
+    List<TimeBooking> findByUserIdAndDate(Long userId, LocalDate date);
+}
